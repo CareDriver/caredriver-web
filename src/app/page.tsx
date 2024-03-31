c53f8574bd52487ea3bfb3fbf048349bb7b1867e
luiggy2620 <luiggymamani@gmail.com>
2024-03-30 21:24:52 -0400

    style: home decorators finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,12 @@ import "@/styles/base/reset.css";
 export default function App() {
     return (
         <main className="home-container">
+            <span className="circles"></span>
             <img src="/images/logowithname.png" alt="" />
             <Link href={"/auth/signin"} className="action-button">
                 Comenzar
             </Link>
+            <span className="circles-right-bottom"></span>
         </main>
     );
 }
