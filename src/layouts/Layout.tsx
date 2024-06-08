6b1e29a64e1fbe6ebeeb7eabfbdb8954c5dbc4fc
luiggy2620 <luiggymamani@gmail.com>
2024-06-08 17:41:46 -0400

    ci: caredriver agent added

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -6,6 +6,7 @@ import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 import AuthProvider from "@/context/AuthContext";
 import Head from "next/head";
+import Script from "next/script";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
@@ -28,6 +29,25 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
                     pauseOnHover
                     theme="light"
                 />
+                <Script id="liveagent-script" strategy="afterInteractive">
+                    {`
+                        (function(d, src, c) {
+                            var t = d.scripts[d.scripts.length - 1], 
+                                s = d.createElement('script'); 
+                            s.id = 'la_x2s6df8d'; 
+                            s.defer = true; 
+                            s.src = src; 
+                            s.onload = s.onreadystatechange = function() {
+                                var rs = this.readyState; 
+                                if (rs && (rs != 'complete') && (rs != 'loaded')) { return; } 
+                                c(this);
+                            }; 
+                            t.parentElement.insertBefore(s, t.nextSibling);
+                        })(document, 'https://caredriver.ladesk.com/scripts/track.js', function(e) {
+                            LiveAgent.createButton('b3cuww8j', e);
+                        });
+                    `}
+                </Script>
             </body>
         </html>
     );
